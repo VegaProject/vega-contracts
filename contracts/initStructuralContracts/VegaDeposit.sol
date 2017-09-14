42a5e7166a03a861d14cc3bba96bd50436ce1949
George <georgek00012@gmail.com>
2017-09-14 14:46:34 -0700

    added init structural contracts, VegaDeposit & VegaFindersFee

---
+++ contracts/initStructuralContracts/VegaDeposit.sol
@@ -0,0 +1,14 @@
+pragma solity 0.4.9;
+
+import '../helpers/Math.sol';
+
+contract VegaDeposit is Math {
+	    
+	
+  ///@notice minDeposit will return the Minimum Deposit needed in VNT to create an allocation contract.
+  ///@param _requestedValue is the amount of value requested to be sent in a allocation contract.
+  function minDeposit(uint _requestedValue) public constant returns (uint) {
+       uint minDeposit = multiplyPercentage(_requestedValue, 1, 1000);
+       return minDeposit;
+   }
+}
