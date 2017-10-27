1b080fbac04bb298dfe5430eb37b7fe60172c69e
George <georgek00012@gmail.com>
2017-10-26 20:54:53 -0400

    adding network config for ropsten-infura

--- truffle.js
+++ truffle.js
@@ -1,5 +1,19 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var infura_apikey = "< insert your infura api key here >";
+var mnemonic = "< insert your metamask 12-word pass phrase here >";
+
 module.exports = {
   networks: {
+    "ropsten": {
+      network_id: 3, // Local Ethereum test network
+      host: "127.0.0.1",
+      port: 8545,
+    },
+    "ropsten-infura": {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+      network_id: 3 // Official Ethereum test network
+    },
     development: {
       host: "localhost",
       port: 8545,
